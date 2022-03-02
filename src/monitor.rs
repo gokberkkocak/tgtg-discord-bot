15441f98d2a804481bc98643f38627ac59289d00
Gökberk KOÇAK <gk34@st-andrews.ac.uk>
2022-03-02 18:07:18 +0000

    Better error handling for monitoring

--- src/monitor.rs
+++ src/monitor.rs
@@ -1,11 +1,14 @@
+use anyhow::Context;
 use serenity::prelude::RwLock;
 use serenity::prelude::TypeMap;
 use serenity::{http::Http, model::id::ChannelId};
 use std::sync::Arc;
 use std::time::Duration;
 use tracing::info;
+use tracing::warn;
 
 use crate::TGTGActiveChannelsContainer;
+use crate::TGTGCredentials;
 use crate::RADIUS_UNIT;
 use crate::{
     CoordinatesWithRadius, ItemMessage, TGTGCredentialsContainer, TGTGItemMessageContainer,
@@ -42,137 +45,20 @@ pub async fn monitor_location(
             if stop_flag {
                 break;
             }
-            let client_data_rw = client_data.write().await;
-            let items =
-                crate::tgtg::get_items(&tgtg_credentials, &coords).expect("Could not get items");
-            info!(
-                "Channel {}: Monitor found {} items",
+            let res = update_location(
+                tgtg_credentials.clone(),
+                client_data.clone(),
+                http.clone(),
                 channel_id,
-                items.len()
-            );
-            for i in items {
-                info!(
-                    "Channel {}: Item {} with quantity {}",
-                    channel_id, i.display_name, i.items_available
+                coords,
+            )
+            .await;
+            if let Err(why) = res {
+                warn!(
+                    "Channel {}: Failed to update location with {}",
+                    channel_id, why
                 );
-                let item_message = {
-                    let item_map = client_data_rw
-                        .get::<TGTGItemMessageContainer>()
-                        .expect("Could not get Item Map")
-                        .read()
-                        .await;
-                    item_map.get(&i.item.item_id).copied()
-                };
-                //  Check if the item is available
-                if i.items_available > 0 {
-                    if let Some(item_message) = item_message {
-                        // Update the message with the new quantity
-                        if item_message.quantity != i.items_available {
-                            channel_id
-                                .edit_message(&http, item_message.message_id, |m| {
-                                    m.embed(|e| {
-                                        e.title(i.store.store_name);
-                                        e.description(i.display_name);
-                                        e.field(
-                                            "Price",
-                                            format!(
-                                                "{:.2} {}",
-                                                i.item.price_including_taxes.minor_units as f64
-                                                    / 10u32
-                                                        .pow(i.item.price_including_taxes.decimals)
-                                                        as f64,
-                                                i.item.price_including_taxes.code
-                                            ),
-                                            true,
-                                        );
-                                        e.field("Quantity", i.items_available, true);
-                                        e.field(
-                                            "Distance",
-                                            format!("{:.2} {}", i.distance, RADIUS_UNIT),
-                                            true,
-                                        );
-                                        e.image(i.store.logo_picture.current_url);
-                                        e
-                                    });
-                                    m
-                                })
-                                .await
-                                .expect("Could not edit message");
-                            let mut item_map = client_data_rw
-                                .get::<TGTGItemMessageContainer>()
-                                .expect("Could not get Item Map")
-                                .write()
-                                .await;
-                            item_map.insert(
-                                i.item.item_id,
-                                ItemMessage {
-                                    message_id: item_message.message_id,
-                                    quantity: i.items_available,
-                                },
-                            );
-                        }
-                    } else {
-                        // We have quantity available, post a new message
-                        let msg = channel_id
-                            .send_message(&http, |m| {
-                                m.embed(|e| {
-                                    e.title(i.store.store_name);
-                                    e.description(i.display_name);
-                                    e.field(
-                                        "Price",
-                                        format!(
-                                            "{:.2} {}",
-                                            i.item.price_including_taxes.minor_units as f64
-                                                / 10u32.pow(i.item.price_including_taxes.decimals)
-                                                    as f64,
-                                            i.item.price_including_taxes.code
-                                        ),
-                                        true,
-                                    );
-                                    e.field("Quantity", i.items_available, true);
-                                    e.field(
-                                        "Distance",
-                                        format!("{:.2} {}", i.distance, RADIUS_UNIT),
-                                        true,
-                                    );
-                                    e.image(i.store.logo_picture.current_url);
-                                    e
-                                });
-                                m
-                            })
-                            .await
-                            .expect("Could not send message");
-                        let mut item_map_write = client_data_rw
-                            .get::<TGTGItemMessageContainer>()
-                            .expect("Could not get Item Map")
-                            .write()
-                            .await;
-                        item_map_write.insert(
-                            i.item.item_id,
-                            ItemMessage {
-                                message_id: msg.id,
-                                quantity: i.items_available,
-                            },
-                        );
-                    }
-                } else {
-                    // No quantity. Check we posted this item before, if yes delete
-                    if let Some(item_message) = item_message {
-                        channel_id
-                            .delete_message(&http, item_message.message_id)
-                            .await
-                            .expect("Could not delete message");
-                        let mut item_map_write = client_data_rw
-                            .get::<TGTGItemMessageContainer>()
-                            .expect("Could not get Item Map")
-                            .write()
-                            .await;
-                        item_map_write.remove(&i.item.item_id);
-                    }
-                }
             }
-            // manually drop lock. we don't want to keep the lock during the sleeping period.
-            drop(client_data_rw);
             tokio::time::sleep(Duration::from_secs(MONITOR_INTERVAL)).await;
         }
         info!(
@@ -180,4 +66,139 @@ pub async fn monitor_location(
             channel_id
         );
     });
+}
+
+async fn update_location(
+    tgtg_credentials: Arc<TGTGCredentials>,
+    client_data: Arc<RwLock<TypeMap>>,
+    http: Arc<Http>,
+    channel_id: ChannelId,
+    coords: CoordinatesWithRadius,
+) -> anyhow::Result<()> {
+    let client_data_rw = client_data.write().await;
+    let items = crate::tgtg::get_items(&tgtg_credentials, &coords)?;
+    info!(
+        "Channel {}: Monitor found {} items",
+        channel_id,
+        items.len()
+    );
+    for i in items {
+        info!(
+            "Channel {}: Item {} with quantity {}",
+            channel_id, i.display_name, i.items_available
+        );
+        let item_message = {
+            let item_map = client_data_rw
+                .get::<TGTGItemMessageContainer>()
+                .context("Item message missing")?
+                .read()
+                .await;
+            item_map.get(&i.item.item_id).copied()
+        };
+        //  Check if the item is available
+        if i.items_available > 0 {
+            if let Some(item_message) = item_message {
+                // Update the message with the new quantity
+                if item_message.quantity != i.items_available {
+                    channel_id
+                        .edit_message(&http, item_message.message_id, |m| {
+                            m.embed(|e| {
+                                e.title(i.store.store_name);
+                                e.description(i.display_name);
+                                e.field(
+                                    "Price",
+                                    format!(
+                                        "{:.2} {}",
+                                        i.item.price_including_taxes.minor_units as f64
+                                            / 10u32.pow(i.item.price_including_taxes.decimals)
+                                                as f64,
+                                        i.item.price_including_taxes.code
+                                    ),
+                                    true,
+                                );
+                                e.field("Quantity", i.items_available, true);
+                                e.field(
+                                    "Distance",
+                                    format!("{:.2} {}", i.distance, RADIUS_UNIT),
+                                    true,
+                                );
+                                e.image(i.store.logo_picture.current_url);
+                                e
+                            });
+                            m
+                        })
+                        .await?;
+                    let mut item_map = client_data_rw
+                        .get::<TGTGItemMessageContainer>()
+                        .context("Item message missing")?
+                        .write()
+                        .await;
+                    item_map.insert(
+                        i.item.item_id,
+                        ItemMessage {
+                            message_id: item_message.message_id,
+                            quantity: i.items_available,
+                        },
+                    );
+                }
+            } else {
+                // We have quantity available, post a new message
+                let msg = channel_id
+                    .send_message(&http, |m| {
+                        m.embed(|e| {
+                            e.title(i.store.store_name);
+                            e.description(i.display_name);
+                            e.field(
+                                "Price",
+                                format!(
+                                    "{:.2} {}",
+                                    i.item.price_including_taxes.minor_units as f64
+                                        / 10u32.pow(i.item.price_including_taxes.decimals) as f64,
+                                    i.item.price_including_taxes.code
+                                ),
+                                true,
+                            );
+                            e.field("Quantity", i.items_available, true);
+                            e.field(
+                                "Distance",
+                                format!("{:.2} {}", i.distance, RADIUS_UNIT),
+                                true,
+                            );
+                            e.image(i.store.logo_picture.current_url);
+                            e
+                        });
+                        m
+                    })
+                    .await?;
+                let mut item_map_write = client_data_rw
+                    .get::<TGTGItemMessageContainer>()
+                    .context("Item message missing")?
+                    .write()
+                    .await;
+                item_map_write.insert(
+                    i.item.item_id,
+                    ItemMessage {
+                        message_id: msg.id,
+                        quantity: i.items_available,
+                    },
+                );
+            }
+        } else {
+            // No quantity. Check we posted this item before, if yes delete
+            if let Some(item_message) = item_message {
+                channel_id
+                    .delete_message(&http, item_message.message_id)
+                    .await?;
+                let mut item_map_write = client_data_rw
+                    .get::<TGTGItemMessageContainer>()
+                    .context("Item message missing")?
+                    .write()
+                    .await;
+                item_map_write.remove(&i.item.item_id);
+            }
+        }
+    }
+    // manually drop lock. we don't want to keep the lock during the sleeping period.
+    drop(client_data_rw);
+    Ok(())
 }
