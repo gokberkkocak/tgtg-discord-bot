b951b47f8af7a7caae650ab1720d3e5a436c341d
Gokberk Kocak <gokberkkocak@gmail.com>
2025-02-02 17:10:17 +0000

    update: deps and migrate pyo3 api

--- src/tgtg.rs
+++ src/tgtg.rs
@@ -1,3 +1,4 @@
+use pyo3::ffi::c_str;
 use pyo3::prelude::*;
 use pyo3::types::{IntoPyDict, PyTuple};
 use tracing::info;
@@ -6,11 +7,11 @@ use crate::data::{TGTGBindings, TGTGConfig, TGTGListing};
 
 pub(crate) fn check_python() -> PyResult<()> {
     Python::with_gil(|py| {
-        let sys = py.import_bound("sys")?;
+        let sys = py.import("sys")?;
         let version: String = sys.getattr("version")?.extract()?;
-        let locals = [("os", py.import_bound("os")?)].into_py_dict_bound(py);
-        let code = "os.getenv('USER') or os.getenv('USERNAME') or 'Unknown'";
-        let user: String = py.eval_bound(code, None, Some(&locals))?.extract()?;
+        let locals = [("os", py.import("os")?)].into_py_dict(py)?;
+        let code = c_str!("os.getenv('USER') or os.getenv('USERNAME') or 'Unknown'");
+        let user: String = py.eval(code, None, Some(&locals))?.extract()?;
         info!(
             "Python status OK! Running user: {}, Python version: {}",
             user, version
@@ -26,19 +27,19 @@ pub fn init_client(
     cookie: &str,
 ) -> PyResult<PyObject> {
     Python::with_gil(|py| {
-        let tgtg_client_fun: Py<PyAny> = PyModule::from_code_bound(
+        let tgtg_client_fun: Py<PyAny> = PyModule::from_code(
             py,
-            "
+            c_str!("
 from tgtg import TgtgClient
 def get_client(access_token, refresh_token, user_id, cookie):
     client = TgtgClient(access_token=access_token, refresh_token=refresh_token, user_id=user_id, cookie=cookie)
-    return client",
-            "",
-            "",
+    return client"),
+            c_str!("client.py"),
+            c_str!("client"),
         )?
         .getattr("get_client")?
         .into();
-        let args = PyTuple::new_bound(py, [&access_token, &refresh_token, &user_id, &cookie]);
+        let args = PyTuple::new(py, [&access_token, &refresh_token, &user_id, &cookie])?;
         let ret: PyObject = tgtg_client_fun.call1(py, args)?;
         Ok(ret)
     })
@@ -46,9 +47,9 @@ def get_client(access_token, refresh_token, user_id, cookie):
 
 pub fn init_fetch_func() -> PyResult<PyObject> {
     Python::with_gil(|py| {
-        let func = PyModule::from_code_bound(
+        let func = PyModule::from_code(
             py,
-            "
+            c_str!("
 import json
 def fetch_items(client, latitude, longitude, radius):
     items = client.get_items(
@@ -58,9 +59,9 @@ def fetch_items(client, latitude, longitude, radius):
         page_size=100,
         radius=radius,
     )
-    return json.dumps(items)",
-            "",
-            "",
+    return json.dumps(items)"),
+    c_str!("fetch.py"),
+    c_str!("fetch"),
         )?
         .getattr("fetch_items")?
         .into();
@@ -71,15 +72,15 @@ def fetch_items(client, latitude, longitude, radius):
 fn py_get_items(tgtg: &TGTGBindings, config: &TGTGConfig) -> PyResult<String> {
     Python::with_gil(|py| {
         let client = tgtg.client.extract(py)?;
-        let params = PyTuple::new_bound(
+        let params = PyTuple::new(
             py,
             [
                 format!("{:.5}", config.latitude),
                 format!("{:.5}", config.longitude),
                 format!("{}", config.radius),
             ],
-        );
-        let args = PyTuple::new_bound(
+        )?;
+        let args = PyTuple::new(
             py,
             [
                 client,
@@ -87,7 +88,7 @@ fn py_get_items(tgtg: &TGTGBindings, config: &TGTGConfig) -> PyResult<String> {
                 params.get_item(1)?,
                 params.get_item(2)?,
             ],
-        );
+        )?;
         let ret = tgtg.fetch_func.call1(py, args)?;
         let items = ret.extract::<String>(py)?;
         Ok(items)
@@ -115,7 +116,7 @@ mod test {
     #[test]
     fn test_tgtg_module() -> PyResult<()> {
         Python::with_gil(|py| {
-            py.import_bound("tgtg")?;
+            py.import("tgtg")?;
             Ok(())
         })
     }
